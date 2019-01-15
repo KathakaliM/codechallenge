e104eab0e8f0e2b78a01d5b63c3c0a018251e8ac
KathakaliM <37589727+KathakaliM@users.noreply.github.com>
2019-01-15 22:49:46 +0530

    Add files via upload

--- src/main/java/com/example/FirstSpringWebApplication1/PageController.java
+++ src/main/java/com/example/FirstSpringWebApplication1/PageController.java
@@ -1,12 +1,16 @@
 package com.example.FirstSpringWebApplication1;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class PageController {
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.example.FirstSpringWebApplication1"})
+public class PageController  {
   
-	@RequestMapping("web")
+	@RequestMapping("/web")
 	public String home()
 	{
 		return "web.jsp";
